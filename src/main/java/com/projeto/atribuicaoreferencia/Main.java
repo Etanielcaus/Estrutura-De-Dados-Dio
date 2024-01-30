87722b7e2eb43bf64c1e125d2b29e52746961677
Etaniel Caus <nielfilosofia@gmail.com>
2024-01-29 22:53:59 -0300

    atribuição e referencia em Java

--- src/main/java/com/projeto/atribuicaoreferencia/Main.java
+++ src/main/java/com/projeto/atribuicaoreferencia/Main.java
@@ -2,6 +2,25 @@ package com.projeto.atribuicaoreferencia;
 
 public class Main {
     public static void main(String[] args) {
-        
+        int intA = 1;
+        int intB = intA;
+
+        System.out.println(intA);
+        System.out.println(intB);
+        System.out.println("=======");
+        intA = 2;
+        System.out.println(intA);
+        System.out.println(intB);
+
+        System.out.println("=======");
+
+        MeuObj objA = new MeuObj(5);
+        MeuObj objB = objA;
+
+        System.out.println(objA);
+        System.out.println(objB);
+        objA.setNum(3);
+        System.out.println(objA);
+        System.out.println(objB);
     }
 }
